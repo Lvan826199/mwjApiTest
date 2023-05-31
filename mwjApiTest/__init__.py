a594191dea5eab99c6a06adf6c551050d7092e2b
van_liu <826199528@qq.com>
2023-05-31 18:51:14 +0800

    Initial commit

---
+++ mwjApiTest/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+@Time : 2023/5/31 16:45
+@Email : Lvan826199@163.com
+@公众号 : 梦无矶的测试开发之路
+@File : __init__.py.py
+'''
+__author__ = "梦无矶小仔"
+
+from .manage import run
---
+++ readme.md
@@ -0,0 +1,7 @@
+# 安装命令
+
+  `
+  pip install MuskTest
+  `
+    
+# 文档建设中...
