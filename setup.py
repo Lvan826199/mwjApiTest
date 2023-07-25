9017baa897fd547153ec2990f1388cfe7d76813d
van_liu <826199528@qq.com>
2023-07-25 16:02:48 +0800

    版本更新1.2.3,修复pip下载部分模板丢失问题

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mwj-apitest",  # 用自己的名替换其中的YOUR_USERNAME_
-    version="1.2.1",  # 包版本号，便于维护版本,保证每次发布都是版本都是唯一的
+    version="1.2.3",  # 包版本号，便于维护版本,保证每次发布都是版本都是唯一的
     author="梦无矶小仔",  # 作者，可以写自己的姓名
     author_email="Lvan826199@163.com",  # 作者联系方式，可写自己的邮箱地址
     description="无需编码的接口测试框架,目前支持json、yaml、py文件格式的用例,命令行mwj查看帮助。",  # 包的简述
@@ -22,6 +22,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Lvan826199/mwjApiTest",  # 自己项目地址，比如github的项目地址
     packages=setuptools.find_packages(),
+    # 包含MANIFEST.in文件所指定的文件
+    include_package_data=True,
     entry_points={
         "console_scripts": ['mwj = mwjApiTest.manage:main']
     },  # 安装成功后，在命令行输入mwj 就相当于执行了mwjApiTest.manage.py中的main了
